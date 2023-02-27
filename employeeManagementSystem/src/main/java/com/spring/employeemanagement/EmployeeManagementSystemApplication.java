9018a8afe78ffa66b1d6c1df87b5e1d47f753f9a
palabhijit687 <palabhijit687@gmail.com>
2023-02-27 14:10:35 +0530

    Employee Management Backend

---
+++ employeeManagementSystem/src/main/java/com/spring/employeemanagement/EmployeeManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.spring.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.spring.employeemanagement.controller")
+public class EmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+	}
+
+}
+
